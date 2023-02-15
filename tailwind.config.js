2a9f7971b765e2dfbe00d33f604bfe8eaae1b2ab
GabrielAF-Faca <gabrielafachim2@gmail.com>
2023-02-15 16:48:22 -0300

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,6 @@ module.exports = {
 	content: ["./src/index.html"],
 	theme: {
 		extend: {},
-		colors: {
-			"nav": "#284b63",
-			"nav-buttons": "#3A6785",
-			"nav-light": "#4380a8",
-			"nav-middle" : "#2c536d",
-			"body": "#353535",
-			"white": "#ffffff",
-			"black": "#000000",
-		},
 	},
 	plugins: [],
 };
