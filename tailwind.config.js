503b2346f702307373d2df491fc278cfa081463b
Your Name <joaovictor.peixoto@yahoo.com.br>
2023-02-15 17:58:38 -0300

    eu sei

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
 	content: ["./src/index.html"],
 	theme: {
 		extend: {},
+		colors: {
+			"nav": "#284b63",
+			"nav-buttons": "#3A6785",
+			"nav-light": "#4380a8",
+			"nav-middle" : "#2c536d",
+			
+			"white": "#ffffff",
+			"black": "#000000",
+		},
 	},
 	plugins: [],
 };
