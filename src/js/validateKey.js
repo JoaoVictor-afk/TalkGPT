e8b28ba89db738f27be9ba920aa1ace238c97c5c
joao victor <joao.souza@ufn.edu.br>
2023-02-24 17:31:47 -0300

    ola

--- src/js/validateKey.js
+++ src/js/validateKey.js
@@ -1,92 +1,75 @@
-
-
-micButtonToggle(false)
+micButtonToggle(false);
 
 if (localStorage.hasOwnProperty("checkbox")) {
-    switch (localStorage.getItem("checkbox")) {
-        case "true":
-            checkbox.checked = true
-            break
-        case "false":
-            checkbox.checked = false
-            break
-        default:
-            checkbox.checked = true
-    }
-    
+	switch (localStorage.getItem("checkbox")) {
+		case "true":
+			checkbox.checked = true;
+			break;
+		case "false":
+			checkbox.checked = false;
+			break;
+		default:
+			checkbox.checked = true;
+	}
 }
 
+var api_key = localStorage.getItem("talkGPTapiKey");
 
-var api_key = localStorage.getItem("talkGPTapiKey")
-
-if (api_key)
-    check_key(api_key)
-
+if (api_key) {
+	check_key(api_key);
+} else {
+	check_key("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");
+}
 
 function validateKey() {
+	const key = key_input.value;
 
-    const key = key_input.value
+	closeApiWindow();
 
-    closeApiWindow()
+	key_input.value = "";
 
-    key_input.value = ""
-
-    check_key(key)
-
+	check_key(key);
 }
 
-
 function check_key(key) {
+	loading.classList.remove("hidden");
+	micButtonToggle(false);
 
-    loading.classList.remove('hidden')
-    micButtonToggle(false)
+	fetch("https://api.openai.com/v1/completions", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${key}`,
+		},
+		body: JSON.stringify({
+			model: "text-ada-001",
+			prompt: "test",
+			max_tokens: 1,
+			n: 1,
+		}),
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			loading.classList.add("hidden");
 
-    fetch('https://api.openai.com/v1/completions', {
-        method: 'POST',
-        headers : {
-            "Content-Type" : "application/json",
-            "Authorization" : `Bearer ${key}` 
-        },
-        body : JSON.stringify({
-            model: "text-ada-001",
-            prompt: "test",
-            max_tokens: 1,
-            n: 1,
-        })
-      })
-        .then(response => response.json())
-        .then(data => {
+			if (!data["error"]) {
+				validation_button.classList.add("bg-green-400");
 
-            loading.classList.add('hidden')
-            
-            if (!data["error"]) {
-                validation_button.classList.add("bg-green-400")
+				micButtonToggle(true);
 
-                micButtonToggle(true)
+				api_key = key;
 
-                api_key = key
+				if (checkbox.checked) {
+					localStorage.setItem("talkGPTapiKey", key);
+				}
+			} else {
+				validation_button.classList.remove("bg-green-400");
 
-                if (checkbox.checked) {
-                
-                    localStorage.setItem("talkGPTapiKey", key)
-                
-                }
-                
-            } else {
-
-                validation_button.classList.remove("bg-green-400")
-
-                micButtonToggle(false)
-
-            }
-            
-            
-        })
-    
+				micButtonToggle(false);
+			}
+		});
 }
 
 function remember(e) {
-
-    localStorage.setItem("checkbox", e.checked)
-
+	localStorage.setItem("checkbox", e.checked);
 }
--- src/js/voice.js
+++ src/js/voice.js
@@ -1,16 +1,15 @@
+navigator.mediaDevices.getUserMedia({ audio: true });
 
-navigator.mediaDevices.getUserMedia({ audio: true })
-
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+window.SpeechRecognition =
+	window.SpeechRecognition || window.webkitSpeechRecognition;
 
 const synth = window.speechSynthesis;
 
 const recognition = new SpeechRecognition();
 recognition.interimResults = true;
 
-var isListening = false
-var isSpeaking = false
-
+var isListening = false;
+var isSpeaking = false;
 
 recognition.addEventListener("result", (e) => {
 	const transcript = Array.from(e.results)
@@ -21,78 +20,64 @@ recognition.addEventListener("result", (e) => {
 	mic_capture.innerHTML = transcript;
 });
 
-
 async function endListen() {
-        
-    const message = mic_capture.innerHTML
+	const message = mic_capture.innerHTML;
 
-    if (message) {
+	if (message) {
+		loading.classList.remove("hidden");
 
-        loading.classList.remove("hidden")
+		console.log(api_key);
 
-        console.log(api_key)
+		fetch("https://api.openai.com/v1/completions", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${api_key}`,
+			},
+			body: JSON.stringify({
+				model: "text-davinci-003",
+				prompt: message,
+				max_tokens: 500,
+				temperature: 0.5,
+				presence_penalty: 0.2,
+				frequency_penalty: 0.2,
+				n: 1,
+			}),
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				let choice = data.choices[0].text;
 
-        fetch("https://api.openai.com/v1/completions", {
-            
-            method : "POST",
-            headers : {
-                "Content-Type" : "application/json",
-                "Authorization" : `Bearer ${api_key}` 
-            },
-            body : JSON.stringify({
+				var result = choice.match(/^[.,:!?]/);
+				if (result != null) {
+					mic_capture.innerHTML += result;
+				}
 
-                model: "text-davinci-003",
-                prompt: message,
-                max_tokens: 500,
-                temperature: 0.5,
-                presence_penalty: 0.2,
-                frequency_penalty: 0.2,
-                n: 1,
+				choice = choice.replace(/^[.,:!?]+/, "");
+				choice = choice.replace(/^(\n)+/, "");
 
-            })
-            
-        }) 
-        .then(response => response.json())
-        .then(data =>{
+				const frase = new SpeechSynthesisUtterance(choice);
+				synth.speak(frase);
 
-            let choice = data.choices[0].text
-            
-            var result = choice.match(/^[.,:!?]/)
-            if (result != null) {
-                mic_capture.innerHTML += result
-            }
-    
-            choice = choice.replace(/^[.,:!?]+/, '')
-            choice = choice.replace(/^(\n)+/, '')
-    
-            const frase = new SpeechSynthesisUtterance(choice);
-            synth.speak(frase);
-    
-    
-            isSpeaking = true
-    
-    
-            button_listen_stop.classList.remove("hidden")
-            loading.classList.add("hidden")
-            chat_answer.classList.remove("hidden")
-    
-            chat_answer.innerHTML = choice
-    
-            typeWrite()
-        })
+				isSpeaking = true;
 
-    }
+				button_listen_stop.classList.remove("hidden");
+				loading.classList.add("hidden");
+				chat_answer.classList.remove("hidden");
 
-    button_listen.classList.remove("text-red-500")
-    button_listen.classList.remove("animate-pulse")
-    button_listen.setAttribute("name", "mic")
+				chat_answer.innerHTML = choice;
 
-    isListening = false
+				typeWrite();
+			});
+	}
 
+	button_listen.classList.remove("text-red-500");
+	button_listen.classList.remove("animate-pulse");
+	button_listen.setAttribute("name", "mic");
+
+	isListening = false;
 }
 
 recognition.addEventListener("end", (e) => {
 	endListen();
 });
-
-
