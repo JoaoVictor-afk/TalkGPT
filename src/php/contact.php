eb16e69130b25ae35bf3a3c5ed6766fb7e32db3f
GabrielAF-Faca <gabrielafachim2@gmail.com>
2023-02-27 17:07:40 -0300

    script php

---
+++ src/php/contact.php
@@ -0,0 +1,35 @@
+
+
+<?php
+
+$name = "";
+$email = "";
+
+if (isset($_POST["name"]) && isset($_POST["email"])){
+    $name = $_POST["name"];
+    $mail = $_POST["email"];
+}
+
+$body = "{
+   name : ".$name.",
+   email : ".$email." 
+}";
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+    CURLOPT_URL => "https://us11.api.mailchimp.com/3.0/lists/825c31743f",
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS =>json_encode($body),
+    CURLOPT_HTTPHEADER => array(
+        "Authorization : 2d43db49313c54b1907a590e0d5e7211-us11",
+    ),
+));
+
+$response = json_decode( curl_exec($curl), $associative );
+
+curl_close($curl);
+
+
+?>
