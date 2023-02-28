9c6c2ea42c5805a35030c0086b6fe5b55721b83e
João Victor Peixoto de Souza <82658732+JoaoVictor-afk@users.noreply.github.com>
2023-02-28 15:07:22 -0300

    Update contact.js

--- src/js/contact.js
+++ src/js/contact.js
@@ -3,32 +3,34 @@ const cta = document.getElementById("cta");
 const sucessnotification = document.getElementById("sucessnotification");
 const failnotification = document.getElementById("failnotification");
 
-cta.addEventListener("click",(event) =>{
-	const email=document.getElementById("email").value;
-	const name=document.getElementById("name").value;
-	const body = {"email":email, "name":name};
+cta.addEventListener("click", (event) => {
+	const email = document.getElementById("email").value;
+	const name = document.getElementById("name").value;
+	const body = { email: email, name: name };
 
 	event.preventDefault();
 	const urlencoded = new URLSearchParams();
 	urlencoded.append("name", name);
 	urlencoded.append("email", email);
 
-	let fetchdata={
-	  method : 'POST',
-	  body :urlencoded,
-	  headers : {"Content-Type":"application/x-www-form-urlencoded"}
-	}
+	let fetchdata = {
+		method: "POST",
+		body: urlencoded,
+		headers: { "Content-Type": "application/x-www-form-urlencoded" },
+	};
 
-	fetch("src/php/contact.php",fetchdata).then(res =>{
-	  if(res.ok){   
-		sucessnotification.classList.remove("hidden");
-		console.log("funcinou")
-	  }else{
-		failnotification.classList.remove("hidden");
-	  }
-	})
-  }
-  );
+	fetch("src/php/contact.php", fetchdata)
+		.then((response) => {
+			if (response.success != false) {
+				sucessnotification.classList.remove("hidden");
+				console.log("funcinou");
+			} else {
+				failnotification.classList.remove("hidden");
+			}
+		})
+		.then((result) => console.log(result))
+		.catch((error) => console.log("error", error));
+});
 
 const closefail = document.getElementById("closefail");
 
