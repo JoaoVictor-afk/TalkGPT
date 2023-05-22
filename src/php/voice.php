66a8e9482de0fa65d2ff930b9ed3260998e887dc
João Victor Peixoto de Souza <82658732+JoaoVictor-afk@users.noreply.github.com>
2023-05-22 15:24:04 -0300

    Update voice.php

--- src/php/voice.php
+++ src/php/voice.php
@@ -2,7 +2,7 @@
 
 $dados = $_POST["dados"];
 
-$key = "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";
+$key = "";
 
 if (isset($_POST["apikey"]) and $_POST["apikey"] != "null")
   $key = $_POST["apikey"];
